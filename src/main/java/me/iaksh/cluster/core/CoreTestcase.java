17f7a885603af0f108f4f547366ba826cf1c0a26
IAKSH <1248675212@qq.com>
2024-01-05 09:59:53 +0800

    点击about page中的图标时（阻塞地）播放core测试例

--- src/main/java/me/iaksh/cluster/core/CoreTestcase.java
+++ src/main/java/me/iaksh/cluster/core/CoreTestcase.java
@@ -344,12 +344,13 @@ public class CoreTestcase {
         return sections;
     }
 
-    /*
+
     public void play() {
         int bpm = 120;
-        //new NESLikeSynthesizer(bpm).saveToWave("./out.wav",genTestSection());
-        new Player().play(0.25f,new NESLikeSynthesizer(bpm).genWaveform(genTestSection()));
+        new Player().play(0.25f, new NESLikeSynthesizer(bpm).genWaveform(genTestSection()));
     }
+
+    /*
     public void save() {
         int bpm = 120;
         new NESLikeSynthesizer(bpm).saveToWave("./out.wav",genTestSection());
--- src/main/java/me/iaksh/cluster/ui/AboutPageController.java
+++ src/main/java/me/iaksh/cluster/ui/AboutPageController.java
@@ -1,5 +1,14 @@
 package me.iaksh.cluster.ui;
 
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import me.iaksh.cluster.core.CoreTestcase;
+
 public class AboutPageController {
 
+    public ImageView aboutPageImage;
+
+    public void onAboutPageImageClick(MouseEvent mouseEvent) {
+        new CoreTestcase().play();
+    }
 }
