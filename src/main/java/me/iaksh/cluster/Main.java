fe61c45ed6660d2f966e6fd25135994ab96cbc55
IAKSH <1248675212@qq.com>
2024-01-10 18:24:35 +0800

    Revert "bring javafx ui back (not finished yet)"
    This reverts commit a098eca6ca7412e7366b5a894c0ca152ce05f15e.

--- src/main/java/me/iaksh/cluster/Main.java
+++ src/main/java/me/iaksh/cluster/Main.java
@@ -1,9 +1,7 @@
 package me.iaksh.cluster;
 
-import me.iaksh.cluster.ui.FXApplication;
-
 public class Main {
     public static void main(String[] args) {
-        FXApplication.runApp();
+        System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module cluster.main {
+    requires java.desktop;
+}
